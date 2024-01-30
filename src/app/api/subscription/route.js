41e447dc7ea3e9dcdbaa4258d075cf9346a60bd8
片倉秀登 <katakurashuto415@KatakuraShutoMacBook-Air.local>
2024-01-30 16:21:01 +0900

    stripeのサブスク支払い画面が表示され、処理も成功した、successページがエラーで表示されない

--- src/app/api/subscription/route.js
+++ src/app/api/subscription/route.js
@@ -1,17 +1,47 @@
+// "use client"
+
 import { NextResponse , NextRequest} from "next/server";
 import Stripe from "stripe";
+
+
+
+// import { NextResponse } from "next/server";
+// import Stripe from "stripe";
+
+// const [prices , setPrices] = useState([]);
+
+export async function GET(request){
+    const stripe = new Stripe('sk_test_51OJs18DeAQc450QQx1df7rQv298LhrEKKTiTXXL5pzC5Wx4HjTQvA7dt3BiD74S5GMBco6bmk0yxc3T1nu8fW47600DijjICiS');
+    const prices = await stripe.prices.list({
+        // lookup_keys:[request.body.lookup_keys],
+        // expand:[data.product],
+    });
+
+    console.log(prices);
+
+    // return NextResponse.json(prices.data.reverse());
+}
+
+
+
+
+
 
 
 
 export async function POST (request) {
     const stripe = new Stripe('sk_test_51OJs18DeAQc450QQx1df7rQv298LhrEKKTiTXXL5pzC5Wx4HjTQvA7dt3BiD74S5GMBco6bmk0yxc3T1nu8fW47600DijjICiS');
     let data = await request.json();
-    let priceId = data.priceId;
+    // let priceId = data.priceId;
+    const prices = await stripe.prices.list({
+        lookup_keys:[request.body.lookup_keys],
+        expand:[data.product],
+    })
     const session = await stripe.checkout.sessions.create({
         billing_address_collection:'auto',
         line_items:[
             {
-                price:priceId,
+                price:prices.data[0].id,
                 quantity:1
             }
         ],
@@ -20,7 +50,7 @@ export async function POST (request) {
         cancel_url:'http://localhost:3000/cancel'
     })
 
-
+    console.log(prices)
     console.log(data);
 
     return NextResponse.json(session.url);
--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,11 +1,23 @@
 // "use client";
 
 // import axios from 'axios';
-// import React, { useEffect, useState } from 'react'
+// import React, { createContext, useContext, useEffect, useState } from 'react'
+// import ProductDisplay from '../components/ProductDisplay/page';
+
+
+
 
 // const Pricing = () => {
     
 //     const [prices , setPrices] = useState([]);
+    
+//     // const PriceContext = createContext({
+//     //     prices,
+//     // })
+
+
+//     const handlePrices = async (e) => {
+
 
 //     useEffect(() => {
 
@@ -14,19 +26,59 @@
 //     },[])
 
 //     const fetchPrices = async () => {
-//         const {data} = await axios.get('/api/getProduct')
+//         const {data} = await axios.get('/api/getProducts')
 //         setPrices(data);
 //         console.log(data);
 //     }
 
+//     }
+
+    
+    
+    
+
 //   return (
 //     <div>
+    
+//     <div>
+//             <form 
+//                 // action="/create-checkout-session"
+//                 // method="POST"
+//                 // type="submit"
+//                 onSubmit={handlePrices}
+//                 >
+//             <input type='hidden' name="lookup_key" value="{{PRICE_LOOKUP_KEY}}" />
+//             <button id="checkout-and-portal-button"
+//                     type="submit"
+                    
+//                     className=' border-2 border-green-600 bg-green-400 rounded-lg w-[150px] h-[50px] hover:scale-110 active:scale-95 mt-[100px]'
+//                     onClick={handlePrices}
+//                     >
+//               Prices
+//             </button>
+//           </form>
+//             </div>
+    
+    
+//     {/* <div>
 //     {prices && prices.map((price) => (
 //         <ProductDisplay price={price} key={price.id} />
 //     ))}
 //     <div>Pricing</div>
+//     </div> */}
+
+    
+
+//     {/* <div>
+//         <PriceContext.Provider value={{prices}}>
+//             <ProductDisplay />
+//         </PriceContext.Provider>
+//     </div> */}
+
 //     </div>
 //   )
 // }
 
 // export default Pricing
+
+
